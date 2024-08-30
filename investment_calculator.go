cc7a5b8be263590af9796e89af0a2096b5817fe0
Ruslan Rakhmedov <rakhmedovrs@gmail.com>
2024-08-30 11:08:20 -0700

    Add function for calculations

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,15 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64) (futureValue float64, futureRealValue float64) {
+	futureValue = investmentAmount * math.Pow(1+expectedReturnRate/float64(100), years)
+	futureRealValue = futureValue / math.Pow(1+inflationRate/float64(100), years)
+	return futureValue, futureRealValue
+}
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var expectedReturnRate float64
 	var years float64
@@ -17,8 +24,7 @@ func main() {
 	fmt.Println("Enter length of years: ")
 	fmt.Scan(&years)
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/float64(100), years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/float64(100), years)
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
 	fmt.Printf("Future value: %.2f\n", futureValue)
 	fmt.Printf("Future real value(adjusted for Inflation): %.2f\n", futureRealValue)
 }
